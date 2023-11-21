8abe5b4d38c468cccd4254487066563b2e42db4b
jingyanzhong <melody804210@gmail.com>
2023-11-21 18:29:55 +0800

    新增header,search

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,122 @@
 <script setup>
+import { ref } from 'vue'
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderComponent from './components/HeaderComponent.vue'
+const searchValue = ref('all')
+const county = ref('縣市')
+const countyShow = ref(false)
+const town = ref('鄉鎮')
+const townShow = ref(false)
+function searchContentShow (val) {
+  if (val === 'county') {
+    countyShow.value = !countyShow.value
+    townShow.value = false
+  } else if (val === 'town') {
+    townShow.value = !townShow.value
+    countyShow.value = false
+  }
+}
+function searchChange (val) {
+  if (val === 'all') {
+    searchValue.value = 'all'
+    county.value = '縣市'
+    town.value = '鄉鎮'
+    countyShow.value = false
+    townShow.value = false
+  } else if (val === 'county') {
+    countyShow.value = false
+    searchValue.value = ''
+  } else if (val === 'town') {
+    townShow.value = false
+  }
+}
 </script>
 
 <template>
   <!-- <RouterLink to="/">首頁</RouterLink> -->
-  <div class="container">
-    <h1>logo</h1>
-
-  </div>
-  <header>
-    <nav>
-
-    </nav>
-  </header>
-
-  <RouterView />
+  <HeaderComponent></HeaderComponent>
+  <main>
+    <div class="container">
+      <div class="search d-flex">
+        <button type="button" class="btn btn-outline-white mr-40" :class="{ 'active': searchValue === 'all' }"
+          @click="searchChange('all')">全國</button>
+          <div class="countyBox">
+            <button type="button" class="county btn btn-outline-white mr-40"
+              @click="searchContentShow('county')">{{ county }}</button>
+            <div class="countyBtnContent" :class="{ 'show': countyShow === true, 'none': countyShow === false }">
+              <label @click="searchChange('county')">
+                <input type="radio" name="county" value="台北市" v-model="county">
+                <span>台北市</span>
+              </label>
+              <label @click="searchChange('county')">
+                <input type="radio" name="county" value="新北市" v-model="county">
+                <span>新北市</span>
+              </label>
+            </div>
+          </div>
+          <div class="townBox">
+            <button type="button" class="town btn btn-outline-white mr-40" @click="searchContentShow('town')">{{ town }}</button>
+            <div class="townBtnContent" :class="{ 'show': townShow === true, 'none': townShow === false }">
+              <label @click="searchChange('town')">
+                <input type="radio" name="town" value="中正區" v-model="town">
+                <span>中正區</span>
+              </label>
+              <label @click="searchChange('town')">
+                <input type="radio" name="town" value="大安區" v-model="town">
+                <span>大安區</span>
+              </label>
+            </div>
+          </div>
+        <input type="search" class="searchText btn btn-outline-white mr-40" placeholder="找地區">
+      </div>
+    </div>
+  </main>
+  <!-- <RouterView /> -->
 </template>
 
 <style lang="scss" scoped>
+.search {
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.countyBox, .townBox {
+  max-width: 255px;
+  width: 100%;
+  margin-right: 40px;
+  position: relative;
+}
+.countyBtnContent,
+.townBtnContent {
+  max-width: 255px;
+  width: 100%;
+  border: 3px solid #fff;
+  border-radius: 16px;
+  color: #fff;
+  background: rgba(78, 67, 118, 1);
+  font-size: 24px;
+  padding: 16px 0;
+  position: absolute;
+  top: 80px;
+  left: 0;
+  z-index: 5;
+  input {
+    display: none;
+  }
+
+  label {
+    display: block;
+    text-align: center;
+    padding: 8px 0;
+    cursor: pointer;
+    transition: all .5s;
+
+    &:hover {
+      background: rgba(255, 255, 255, .5);
+    }
+  }
+}
+.searchText {
+  width: calc(100% - (255px * 3 + 160px));
+  min-width: 255px;
+}
 </style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,199 @@
+<script setup>
+import { ref } from 'vue'
+const text = ref('15')
+</script>
+
+<template>
+    <header>
+        <div class="container d-flex align-items-center">
+            <h1>
+                <RouterLink to="/">
+                    總統即時開票全台地圖
+                </RouterLink>
+            </h1>
+            <nav class="position-relative">
+                <div class="line bg-primary"></div>
+                <a href="#" class="circle circle1 active position-absolute" @click="text = '15'">
+                    <div class="text position-absolute d-flex">
+                        <span>2020</span>
+                        <span class="text-m active-none moblie-none"
+                            :class="{ 'active-show': text === '15' }">第15任總統副總選舉</span>
+                    </div>
+                </a>
+                <a href="#" class="circle circle2 position-absolute start-50" @click="text = '14'">
+                    <div class="text position-absolute d-flex">
+                        <span>2016</span>
+                        <span class="text-m active-none moblie-none"
+                            :class="{ 'active-show': text === '14' }">第14任總統副總選舉</span>
+                    </div>
+                </a>
+                <a href="#" class="circle circle3 position-absolute" @click="text = '13'">
+                    <div class="text position-absolute d-flex">
+                        <span>2012</span>
+                        <span class="text-m active-none moblie-none"
+                            :class="{ 'active-show': text === '13' }">第13任總統副總選舉</span>
+                    </div>
+                </a>
+                <div class="moblie-text">
+                    <span>第{{ text }}任總統副總選舉</span>
+                </div>
+            </nav>
+        </div>
+    </header>
+</template>
+<style lang="scss" scoped>
+.active-none {
+    display: none;
+}
+
+.active-show {
+    display: block;
+}
+
+.moblie-text {
+    display: none;
+}
+
+header {
+    margin-bottom: 40px;
+    padding: 50px 0;
+    background: linear-gradient(175deg, rgba(217, 217, 217, 1), rgba(217, 217, 217, 0));
+}
+
+h1 {
+    margin-right: 90px;
+
+    a {
+        display: block;
+        background: url(../img/logo.png);
+        width: 100px;
+        height: 100px;
+        overflow: hidden;
+        text-wrap: nowrap;
+        text-indent: 101%;
+    }
+}
+
+nav {
+    width: 100%;
+}
+
+.line {
+    width: 100%;
+    height: 8px;
+    margin-top: 47px;
+    border-radius: 5px;
+}
+
+.circle {
+    width: 40px;
+    height: 40px;
+    border-radius: 20px;
+    top: 30px;
+    transition: all .5s;
+    background: rgba(78, 67, 118, 1);
+
+    &:hover {
+        background: rgba(133, 97, 139, 1);
+
+        span {
+            color: rgba(133, 97, 139, 1);
+        }
+    }
+
+    .text {
+        top: -50px;
+        left: 50%;
+        transform: translateX(-50%);
+        font-size: 32px;
+    }
+
+    span {
+        color: rgba(78, 67, 118, 1);
+        text-wrap: nowrap;
+        transition: all .5s;
+    }
+}
+
+.circle1 {
+    left: 15%;
+}
+
+.circle2 {
+    transform: translateX(-50%);
+}
+
+.circle3 {
+    right: 15%;
+}
+
+@media(max-width: 1280px) {
+    .circle {
+        .text {
+            font-size: 24px;
+        }
+    }
+
+    .circle1 {
+        left: 18%;
+    }
+
+    .circle3 {
+        right: 18%;
+    }
+}
+
+@media(max-width: 768px) {
+    .moblie-none {
+        display: none;
+    }
+
+    .moblie-text {
+        display: block;
+        position: absolute;
+        bottom: -60px;
+        font-size: 24px;
+        color: rgba(78, 67, 118, 1);
+    }
+
+    header {
+        padding: 40px 0;
+    }
+
+    h1 {
+        margin-right: 16px;
+
+        a {
+            width: 60px;
+            height: 60px;
+            background-size: cover;
+        }
+    }
+
+    .line {
+        margin-top: 0px;
+        border-radius: 5px 0 0 5px;
+    }
+
+    .circle {
+        top: -16px;
+
+        .text {
+            font-size: 24px;
+            top: -36px;
+        }
+
+        .text-m {
+            position: absolute;
+            bottom: -81px;
+        }
+    }
+
+    .circle1 {
+        left: 10%;
+    }
+
+    .circle3 {
+        right: 10%;
+    }
+}</style>
